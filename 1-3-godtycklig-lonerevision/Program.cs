984b4111c76725db36cb74d375b014696fc2dac2
fr222cy <fr222cy@student.lnu.se>
2014-09-20 16:00:45 +0200

    First stage, bases done, methods etc

---
+++ 1-3-godtycklig-lonerevision/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_3_godtycklig_lonerevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfSalaries = Readint("Enter the amount of Salaries");
+            processSalaries(amountOfSalaries);
+
+        }
+        static void processSalaries(int count)
+        {
+            
+              if (count <= 2)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Du måste mata in minst två löner för att kunna göra en beräkning!" );
+                Console.ResetColor();
+                return;
+            }
+              int[] array = new int[count];
+
+              for (int i = 0; i < count; i++)
+              {
+                  Console.Write("Mata in lön nr{0}", i);
+                  array[i] = int.Parse(Console.ReadLine());
+                  
+              }
+              Console.WriteLine(array[3]);
+        }
+
+        static int Readint(string prompt)
+        {
+            Console.Write("Enter the amount of salaries: ");
+            int amountOfSalaries = int.Parse(Console.ReadLine());
+            return amountOfSalaries;
+        }
+    }
+}
